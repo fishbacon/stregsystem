a90f034c76e9703484febe67c5b95f8f5dbca3fb
Mark Faldborg <MarkFaldborg@gmail.com>
2015-02-25 17:48:06 +0100

    Corrected bad test.

--- stregsystem/stregsystem/test/test_utils.py
+++ stregsystem/stregsystem/test/test_utils.py
@@ -8,7 +8,7 @@ class TestGrouper(TestCase):
 
 class TestPhoneLetterConverter(TestCase):
     def test_phone_letter_converter_lower(self):
-        self.assertEqual(utils.phone_letter_converter('5052treo'), '50527625')
+        self.assertEqual(utils.phone_letter_converter('5052treo'), '50528736')
 
     def test_phone_letter_converter_upper(self):
-        self.assertEqual(utils.phone_letter_converter('5052TREO'), '50527625')
+        self.assertEqual(utils.phone_letter_converter('5052TREO'), '50528736')
